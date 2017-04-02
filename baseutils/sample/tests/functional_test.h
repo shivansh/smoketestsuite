fd7ff610ae579bbe985f9af2554763abecfe3915
Shivansh Rai <shivansh@freebsd.org>
2017-04-03 01:07:56 +0530

    Update comments

--- baseutils/sample/tests/functional_test.h
+++ baseutils/sample/tests/functional_test.h
@@ -8,6 +8,10 @@ int opt;
 int option_index = 0;     /* longindex */
 int flag = 0;             /* flipped when a valid argument is found */
 
+/* The following short and long options are supposed to
+ * be specific to the "sample" base utility under test
+ * and provide an exemplary workflow which will be used
+ */
 char short_options[] = "";
 
 const struct option long_options[] = {
