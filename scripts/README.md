f1636e11f7fa1f83cbcb6579fd24be5ef8fcb611
Shivansh Rai <shivansh@freebsd.org>
2017-04-06 12:28:50 +0530

    Add script for listing all base utilities without tests
    "utils_list" will be later parsed when generating tests
    Fixes #1

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# Handy scripts
+
+Script Name | Functionality
+----------- | -------------
+[fetch_utils.sh](fetch_utils.sh) | Lists all the base utilities currently without tests and saves in **utils_list**
---
+++ scripts/fetch_utils.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# Script for listing all the base utilities currently without tests
+
+check_utils() {
+  printf "=========\n"
+  basename "$path"
+  printf "=========\n"
+  cd "$path" || exit
+  for dir in */;
+  do
+    (
+      cd "$dir" || exit
+      if [ ! -d "tests" ]; then
+        basename "$dir"
+      fi
+    )
+  done
+}
+
+rm -f utils_list
+
+path="$HOME/source-codes/freebsd/bin"
+(check_utils) >> utils_list
+
+path="$HOME/source-codes/freebsd/sbin"
+(check_utils) >> utils_list
