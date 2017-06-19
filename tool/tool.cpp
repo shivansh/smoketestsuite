f4e3fb5cbaac5a0114945d53d503e60531fb6fa1
Shivansh Rai <shivansh@freebsd.org>
2017-06-19 13:48:53 +0500

    Add namespace declaration and relevant modifications

--- tool/tool.cpp
+++ tool/tool.cpp
@@ -5,44 +5,39 @@
 #include <unordered_map>
 #include "tool.hpp"
 
-using namespace std;
-
-int
-main()
+tool::insert_opts::insert_opts()
 {
-	string line; 				// An individual line in a man-page.
-	string opt_ident = ".It Fl"; 		// Identifier for an option in man page.
-	string opt_name; 			// Name of the option.
-	int opt_pos; 				// Starting index of the (identified) option.
-	list<opt_def> opt_list; 		// List of identified option definitions (opt_def's).
-	list<opt_def>::iterator opt_list_iter;
-	unordered_map<string, opt_def> opt_map; // Map "option value" to "option definition".
-	unordered_map<string, opt_def>::iterator opt_map_iter;
-
 	// Option definitions.
-	opt_def h_def; 		// '-h'
+	opt_def h_def;				// '-h'
 	h_def.type = 's';
 	h_def.value = "h";
 	h_def.keyword = "help";
-	opt_def v_def; 		// '-v'
+	opt_def v_def;				// '-v'
 	v_def.type = 's';
 	v_def.value = "v";
 	v_def.keyword = "version";
 
 	// `opt_map` contains all the options
 	// which can be "easily" tested.
-	opt_map.insert(make_pair<string, opt_def>
-				("h", (opt_def)h_def));  // NOTE: Explicit typecast required here.
-	opt_map.insert(make_pair<string, opt_def>
-				("v", (opt_def)v_def));
+	opt_map.insert(std::make_pair<std::string, opt_def>
+			("h", (opt_def)h_def));		// NOTE: Explicit typecast required here.
+	opt_map.insert(std::make_pair<std::string, opt_def>
+			("v", (opt_def)v_def));
+
+};
+
+int
+main()
+{
+	using namespace tool;
 
 	// An example man-page for ln(1).
-	ifstream infile("ln.1");
+	std::ifstream infile("ln.1");
 
 	// Search for all the options accepted by the
 	// utility and collect those present in `opt_map`.
-	while (getline(infile, line)) {
-		if ((opt_pos = line.find(opt_ident)) != string::npos) {
+	while (std::getline(infile, line)) {
+		if ((opt_pos = tool::line.find(opt_ident)) != std::string::npos) {
 			opt_pos += opt_ident.length() + 1; 	// Locate the position of option name.
 			opt_name = line.substr(opt_pos);
 			if ((opt_map_iter = opt_map.find(opt_name)) != opt_map.end()) {
@@ -52,8 +47,8 @@ main()
 	}
 
 	for (auto it : opt_list)
-		cout << it.value << ": "
-		     << it.keyword << endl; 	// NOTE: Test operation, will be updated.
+		std::cout << it.value << ": "
+							<< it.keyword << std::endl; 	// NOTE: Test operation, will be updated.
 
 	return 0;
 }
