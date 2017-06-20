c2b5c7bca8b7c2d859cacfdce1601b377cd86934
Shivansh Rai <shivansh@freebsd.org>
2017-06-20 23:02:09 +0530

    Shift back to a tabstop value of 2.
    Indentations according to style(9) can be done afterwards if required.

--- tool/tool.cpp
+++ tool/tool.cpp
@@ -7,48 +7,48 @@
 
 tool::insert_opts::insert_opts()
 {
-	// Option definitions.
-	opt_def h_def;				// '-h'
-	h_def.type = 's';
-	h_def.value = "h";
-	h_def.keyword = "help";
-	opt_def v_def;				// '-v'
-	v_def.type = 's';
-	v_def.value = "v";
-	v_def.keyword = "version";
+  // Option definitions.
+  opt_def h_def;        // '-h'
+  h_def.type = 's';
+  h_def.value = "h";
+  h_def.keyword = "help";
+  opt_def v_def;        // '-v'
+  v_def.type = 's';
+  v_def.value = "v";
+  v_def.keyword = "version";
 
-	// `opt_map` contains all the options
-	// which can be "easily" tested.
-	opt_map.insert(std::make_pair<std::string, opt_def>
-			("h", (opt_def)h_def));		// NOTE: Explicit typecast required here.
-	opt_map.insert(std::make_pair<std::string, opt_def>
-			("v", (opt_def)v_def));
+  // `opt_map` contains all the options
+  // which can be "easily" tested.
+  opt_map.insert(std::make_pair<std::string, opt_def>
+                               ("h", (opt_def)h_def));  // NOTE: Explicit typecast required here.
+  opt_map.insert(std::make_pair<std::string, opt_def>
+                               ("v", (opt_def)v_def));
 
 };
 
 int
 main()
 {
-	using namespace tool;
+  using namespace tool;
 
-	// An example man-page for ln(1).
-	std::ifstream infile("ln.1");
+  // An example man-page for ln(1).
+  std::ifstream infile("ln.1");
 
-	// Search for all the options accepted by the
-	// utility and collect those present in `opt_map`.
-	while (std::getline(infile, line)) {
-		if ((opt_pos = tool::line.find(opt_ident)) != std::string::npos) {
-			opt_pos += opt_ident.length() + 1; 	// Locate the position of option name.
-			opt_name = line.substr(opt_pos);
-			if ((opt_map_iter = opt_map.find(opt_name)) != opt_map.end()) {
-				opt_list.push_back(opt_map_iter->second);
-			}
-		}
-	}
+  // Search for all the options accepted by the
+  // utility and collect those present in `opt_map`.
+  while (std::getline(infile, line)) {
+    if ((opt_pos = tool::line.find(opt_ident)) != std::string::npos) {
+      opt_pos += opt_ident.length() + 1;    // Locate the position of option name.
+      opt_name = line.substr(opt_pos);
+      if ((opt_map_iter = opt_map.find(opt_name)) != opt_map.end()) {
+        opt_list.push_back(opt_map_iter->second);
+      }
+    }
+  }
 
-	for (auto it : opt_list)
-		std::cout << it.value << ": "
-							<< it.keyword << std::endl; 	// NOTE: Test operation, will be updated.
+  for (auto it : opt_list)
+    std::cout << it.value << ": "
+              << it.keyword << std::endl;   // NOTE: Test operation, will be updated.
 
-	return 0;
+  return 0;
 }
--- tool/tool.hpp
+++ tool/tool.hpp
@@ -1,22 +1,22 @@
 // Basic option defition.
 typedef struct opt_def {
-	char type;						// Option type: (s)short/(l)long.
-	std::string value;
-	std::string keyword; 	// The keyword which should be looked up in the
-												// message (if) produced when using this option.
+  char type;            // Option type: (s)short/(l)long.
+  std::string value;
+  std::string keyword;  // The keyword which should be looked up in the
+                        // message (if) produced when using this option.
 } opt_def;
 
 namespace tool {
-	std::string line;										// An individual line in a man-page.
-	std::string opt_ident = ".It Fl"; 	// Identifier for an option in man page.
-	std::string opt_name;								// Name of the option.
-	int opt_pos;												// Starting index of the (identified) option.
-	std::list<opt_def> opt_list;				// List of identified option definitions (opt_def's).
-	std::list<opt_def>::iterator opt_list_iter;
-	std::unordered_map<std::string, opt_def> opt_map; // Map "option value" to "option definition".
-	std::unordered_map<std::string, opt_def>::iterator opt_map_iter;
+  std::string line;                   // An individual line in a man-page.
+  std::string opt_ident = ".It Fl"; 	// Identifier for an option in man page.
+  std::string opt_name;               // Name of the option.
+  int opt_pos;                        // Starting index of the (identified) option.
+  std::list<opt_def> opt_list;        // List of identified option definitions (opt_def's).
+  std::list<opt_def>::iterator opt_list_iter;
+  std::unordered_map<std::string, opt_def> opt_map; // Map "option value" to "option definition".
+  std::unordered_map<std::string, opt_def>::iterator opt_map_iter;
 
-	class insert_opts {
-		insert_opts();
-	};
+  class insert_opts {
+    insert_opts();
+  };
 }
